93af436914f0782eb05047ec5b25a87b439d63e8
shinel98 <inhyeok38@gmail.com>
2022-01-28 14:35:47 +0900

    Merge branch 'master' of https://github.com/JihuYang/MBTI-Project.git

--- src/main/java/com/hgu/webcamp/HomeController.java
+++ src/main/java/com/hgu/webcamp/HomeController.java
@@ -1,27 +1,41 @@
 package com.hgu.webcamp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.hgu.webcamp.DTO.commentDTO;
+import com.hgu.webcamp.Service.userService;
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
 	
-	
+	@Autowired
+	userService userService;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
+	public ModelAndView home(Model model) {
+		ModelAndView mv = new ModelAndView();
 		commentDTO dto = new commentDTO();
 		System.out.println("WELCOME TO MBTI WEB SITE");
 		dto.toString();
-		return "index";
+		
+		int[] views = 	{0,0,0,0,0};
+		
+		for(int i=0; i<5; i++)
+			views[i] = userService.readViews(i+1);
+
+		mv.addObject("views", views);
+		mv.setViewName("index");
+		return mv;
 	}
 	
 }
