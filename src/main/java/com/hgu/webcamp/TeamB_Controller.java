ca8a091f85751fab008e5a6a852d4f3a2e993d1a
susannajung <21800662@handong.edu>
2022-03-04 02:27:50 +0900

    teamBcontroller add return 주소 수정

--- src/main/java/com/hgu/webcamp/TeamB_Controller.java
+++ src/main/java/com/hgu/webcamp/TeamB_Controller.java
@@ -184,11 +184,8 @@ public class TeamB_Controller {
 		if(i == 0) System.out.println("Error! delete Failed");
 		else System.out.println("댓글 삭제 완료.");
 		
-<<<<<<< HEAD
-		return "redirect:<%=request.getContextPath()%>/result";
-=======
+//		return "redirect:<%=request.getContextPath()%>/result";
 		return "redirect:/teamB/start";
->>>>>>> branch 'master' of https://github.com/JihuYang/MBTI-Project.git
 	}
 	
 	@RequestMapping(value = "/addok", method = RequestMethod.POST)
